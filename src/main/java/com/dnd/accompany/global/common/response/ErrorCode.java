bef0071e0a25a99988f6c003eb807ee04c47e159
min429 <lsm990429@gmail.com>
2024-08-04 17:32:32 +0900

    feat: 동행글과 관련된 예외 및 코드를 추가한다.

--- src/main/java/com/dnd/accompany/global/common/response/ErrorCode.java
+++ src/main/java/com/dnd/accompany/global/common/response/ErrorCode.java
@@ -25,7 +25,11 @@ public enum ErrorCode {
 	INVALID_OAUTH_TOKEN(MatripConstant.BAD_REQUEST, "LOGIN-002", "유효하지 않은 OAuth 토큰입니다."),
 
 	// ---- 네트워크 ---- //
-	HTTP_CLIENT_REQUEST_FAILED(MatripConstant.INTERNAL_SERVER_ERROR, "NETWORK-001", "서버 요청에 실패하였습니다.");
+	HTTP_CLIENT_REQUEST_FAILED(MatripConstant.INTERNAL_SERVER_ERROR, "NETWORK-001", "서버 요청에 실패하였습니다."),
+
+	// ---- 동행글 ---- //
+	ACCOMPANY_BOARD_NOT_FOUND(MatripConstant.NOT_FOUND, "ACCOMPANY_BOARD-001", "동행글을 찾을 수 없습니다."),
+	ACCOMPANY_BOARD_ACCESS_DENIED(MatripConstant.FORBIDDEN, "ACCOMPANY_BOARD-002", "동행글 접근 권한이 없습니다.");
 
 	private final Integer status;
 	private final String code;
