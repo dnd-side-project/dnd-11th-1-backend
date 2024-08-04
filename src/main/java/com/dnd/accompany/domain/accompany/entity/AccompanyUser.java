743184f28187fa57aefa13ffe037ad5ab33d43a8
min429 <lsm990429@gmail.com>
2024-08-04 17:29:55 +0900

    chore: 파일 이름 변경으로 인해 엔티티를 다시 추가한다.

---
+++ src/main/java/com/dnd/accompany/domain/accompany/entity/AccompanyUser.java
@@ -0,0 +1,63 @@
+package com.dnd.accompany.domain.accompany.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.dnd.accompany.domain.accompany.entity.enums.Role;
+import com.dnd.accompany.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "accompany_users", indexes = {
+	@Index(name = "IX_user_id", columnList = "user_id"),
+	@Index(name = "IX_accompany_boards_id", columnList = "accompany_boards_id")
+})
+@SQLRestriction("deleted = false")
+@SQLDelete(sql = "UPDATE t_order SET deleted = true WHERE id = ?")
+public class AccompanyUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "accompany_users_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "accompany_boards_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	private AccompanyBoard accompanyBoard;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
+	@Builder
+	public AccompanyUser(Long id, User user, AccompanyBoard accompanyBoard, Role role) {
+		this.id = id;
+		this.user = user;
+		this.accompanyBoard = accompanyBoard;
+		this.role = role;
+	}
+}
