b84d011e9d9a2e1d84f025b572120fff091fbdd5
min429 <lsm990429@gmail.com>
2024-08-06 15:12:55 +0900

    refactor: AccompanyRequestRequest의 이름을 수정한다.

--- src/main/java/com/dnd/accompany/domain/accompany/api/dto/CreateAccompanyRequest.java
+++ src/main/java/com/dnd/accompany/domain/accompany/api/dto/CreateAccompanyRequest.java
@@ -3,7 +3,7 @@ package com.dnd.accompany.domain.accompany.api.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record AccompanyRequestRequest(
+public record CreateAccompanyRequest(
 	@NotNull Long boardId,
 	@NotNull @Size(max = 1500) String introduce,
 	@NotNull String chatLink
