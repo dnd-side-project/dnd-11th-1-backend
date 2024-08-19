c86c7652b3849a018a040c5579aba12a446414eb
min429 <lsm990429@gmail.com>
2024-08-19 20:40:02 +0900

    feat: 동행 신청서 조회 관련 repository를 수정한다.

--- src/main/java/com/dnd/accompany/domain/accompany/infrastructure/AccompanyUserRepository.java
+++ src/main/java/com/dnd/accompany/domain/accompany/infrastructure/AccompanyUserRepository.java
@@ -12,6 +12,9 @@ import com.dnd.accompany.domain.accompany.infrastructure.querydsl.interfaces.Acc
 public interface AccompanyUserRepository extends JpaRepository<AccompanyUser, Long>, AccompanyUserRepositoryCustom {
 	void deleteByAccompanyBoardId(Long boardId);
 
-	@Query("SELECT u.user.id FROM AccompanyUser u WHERE u.accompanyBoard.id = :boardId")
+	@Query("SELECT u.user.id FROM AccompanyUser u WHERE u.accompanyBoard.id = :boardId AND u.role = 'HOST'")
 	Optional<Long> findUserIdByAccompanyBoardId(Long boardId);
+
+	@Query("SELECT u.user.id FROM AccompanyUser u WHERE u.accompanyBoard.id = :boardId AND u.role = 'HOST'")
+	Optional<Long> findHostIdByAccompanyBoardId(Long boardId);
 }
