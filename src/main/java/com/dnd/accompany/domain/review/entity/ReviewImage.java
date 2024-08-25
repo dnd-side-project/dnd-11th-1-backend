3a84196aa0b044f7f38dab366337e89fddf92e16
rlarltj <rlarltj1234@gmail.com>
2024-08-26 03:43:42 +0900

    feat: Review의 컬렉션 필드들을 테이블로 분리한다.

---
+++ src/main/java/com/dnd/accompany/domain/review/entity/ReviewImage.java
@@ -0,0 +1,46 @@
+package com.dnd.accompany.domain.review.entity;
+
+import com.dnd.accompany.domain.common.entity.TimeBaseEntity;
+import com.dnd.accompany.domain.review.entity.enums.PersonalityType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Builder
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "review_images")
+@SQLRestriction("deleted = false")
+@SQLDelete(sql = "UPDATE review_images SET deleted = true WHERE id = ?")
+public class ReviewImage extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    private boolean deleted = Boolean.FALSE;
+}
