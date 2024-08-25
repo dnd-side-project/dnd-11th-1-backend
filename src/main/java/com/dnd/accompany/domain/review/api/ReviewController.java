3124682871cdf2ad7879a5657b4599e740933b2b
rlarltj <rlarltj1234@gmail.com>
2024-08-26 03:44:39 +0900

    feat: Review를 상세 조회하는 api를 추가한다.

--- src/main/java/com/dnd/accompany/domain/review/api/ReviewController.java
+++ src/main/java/com/dnd/accompany/domain/review/api/ReviewController.java
@@ -2,6 +2,8 @@ package com.dnd.accompany.domain.review.api;
 
 import com.dnd.accompany.domain.auth.dto.jwt.JwtAuthentication;
 import com.dnd.accompany.domain.review.api.dto.CreateReviewRequest;
+import com.dnd.accompany.domain.review.api.dto.ReviewDetailsResponse;
+import com.dnd.accompany.domain.review.api.dto.ReviewDetailsResult;
 import com.dnd.accompany.domain.review.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,6 +11,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +35,26 @@ public class ReviewController {
         Long id = reviewService.create(user.getId(), request);
 
         return ResponseEntity.ok(id);
+    }
+
+    @Operation(summary = "리뷰 상세 조회")
+    @GetMapping("/{id}")
+    public ResponseEntity<ReviewDetailsResponse> getDetails(
+            @AuthenticationPrincipal JwtAuthentication user,
+            @PathVariable("id") Long reviewId
+    ) {
+        ReviewDetailsResult result = reviewService.getReviewDetails(user.getId(), reviewId);
+        ReviewDetailsResponse response = ReviewDetailsResponse.of(result);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "받은 리뷰 목록 조회")
+    @GetMapping
+    public ResponseEntity<ReviewDetailsResponse> getReviewList(
+            @AuthenticationPrincipal JwtAuthentication user
+    ) {
+        reviewService.getReviewList(user.getId());
+        return null;
     }
 }
--- src/main/java/com/dnd/accompany/domain/review/service/ReviewService.java
+++ src/main/java/com/dnd/accompany/domain/review/service/ReviewService.java
@@ -1,14 +1,19 @@
 package com.dnd.accompany.domain.review.service;
 
+import com.dnd.accompany.domain.accompany.entity.AccompanyBoard;
 import com.dnd.accompany.domain.accompany.infrastructure.AccompanyBoardRepository;
 import com.dnd.accompany.domain.review.api.dto.CreateReviewRequest;
+import com.dnd.accompany.domain.review.api.dto.ReviewDetailsResult;
 import com.dnd.accompany.domain.review.entity.Review;
 import com.dnd.accompany.domain.review.infrastructure.ReviewRepository;
+import com.dnd.accompany.domain.user.entity.User;
 import com.dnd.accompany.domain.user.infrastructure.UserRepository;
+import com.dnd.accompany.global.common.exception.BadRequestException;
 import com.dnd.accompany.global.common.exception.NotFoundException;
 import com.dnd.accompany.global.common.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +23,15 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final AccompanyBoardRepository accompanyBoardRepository;
 
+    @Transactional
     public Long create(Long userId, CreateReviewRequest request) {
-        validateWriter(userId);
-        validateAccompanyBoard(request.accompanyBoardId());
+        getWriter(userId);
 
-        Review review = Review.of(
+        getAccompanyBoard(request.accompanyBoardId());
+
+        Review review = Review.createReview(
                 userId,
+                request.receiverId(),
                 request.accompanyBoardId(),
                 request.satisfactionLevel(),
                 request.recommendationStatus(),
@@ -40,13 +48,41 @@ public class ReviewService {
         return review.getId();
     }
 
-    private void validateWriter(Long userId) {
-        userRepository.findById(userId)
+    @Transactional(readOnly = true)
+    public ReviewDetailsResult getReviewDetails(Long userId, Long reviewId) {
+        Review review = getReview(reviewId);
+
+        validateReceiver(userId, review);
+
+        User writer = getWriter(review.getWriterId());
+        AccompanyBoard accompanyBoard = getAccompanyBoard(review.getAccompanyBoardId());
+
+        return ReviewDetailsResult.of(writer, review, accompanyBoard);
+    }
+
+    @Transactional(readOnly = true)
+    public void getReviewList(Long userId) {
+        //TODO
+    }
+
+    private Review getReview(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
+    }
+
+    private User getWriter(Long userId) {
+        return userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
     }
 
-    private void validateAccompanyBoard(Long accompanyBoardId) {
-        accompanyBoardRepository.findById(accompanyBoardId)
+    private AccompanyBoard getAccompanyBoard(Long accompanyBoardId) {
+        return accompanyBoardRepository.findById(accompanyBoardId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.ACCOMPANY_BOARD_NOT_FOUND));
+    }
+
+    private void validateReceiver(Long userId, Review review) {
+        if (!userId.equals(review.getReceiverId())) {
+            throw new BadRequestException(ErrorCode.ACCESS_DENIED);
+        }
     }
 }
