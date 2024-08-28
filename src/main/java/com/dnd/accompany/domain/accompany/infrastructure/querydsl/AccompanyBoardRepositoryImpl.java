4e383db5104a5ec61e5e16ff050753011dc30669
min429 <lsm990429@gmail.com>
2024-08-29 00:32:08 +0900

    feat: 커서 기반 페이징 방식으로 쿼리를 재구성한다.

--- src/main/java/com/dnd/accompany/domain/accompany/infrastructure/querydsl/AccompanyBoardRepositoryImpl.java
+++ src/main/java/com/dnd/accompany/domain/accompany/infrastructure/querydsl/AccompanyBoardRepositoryImpl.java
@@ -1,19 +1,24 @@
 package com.dnd.accompany.domain.accompany.infrastructure.querydsl;
 
+import static com.dnd.accompany.domain.accompany.api.dto.FindSlicesResult.*;
+import static com.dnd.accompany.domain.accompany.api.dto.PageRequest.*;
 import static com.dnd.accompany.domain.accompany.entity.QAccompanyBoard.*;
 import static com.dnd.accompany.domain.accompany.entity.QAccompanyImage.*;
+import static com.dnd.accompany.domain.accompany.entity.QAccompanyRequest.*;
 import static com.dnd.accompany.domain.accompany.entity.QAccompanyUser.*;
 import static com.dnd.accompany.domain.accompany.entity.enums.Role.*;
 import static com.dnd.accompany.domain.user.entity.QUser.*;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import com.dnd.accompany.domain.accompany.api.dto.FindBoardThumbnailsResult;
+import com.dnd.accompany.domain.accompany.api.dto.FindSlicesResult;
 import com.dnd.accompany.domain.accompany.entity.enums.Region;
 import com.dnd.accompany.domain.accompany.infrastructure.querydsl.interfaces.AccompanyBoardRepositoryCustom;
 import com.querydsl.core.BooleanBuilder;
@@ -44,7 +49,7 @@ public class AccompanyBoardRepositoryImpl implements AccompanyBoardRepositoryCus
 	}
 
 	@Override
-	public Slice<FindBoardThumbnailsResult> findBoardThumbnails(Pageable pageable, Region region) {
+	public Slice<FindBoardThumbnailsResult> findBoardThumbnails(String cursor, int size, Region region) {
 		List<FindBoardThumbnailsResult> content = queryFactory
 			.select(Projections.constructor(FindBoardThumbnailsResult.class,
 				accompanyBoard.id,
@@ -53,18 +58,24 @@ public class AccompanyBoardRepositoryImpl implements AccompanyBoardRepositoryCus
 				accompanyBoard.startDate,
 				accompanyBoard.endDate,
 				user.nickname,
-				Expressions.stringTemplate("GROUP_CONCAT(DISTINCT {0})", accompanyImage.imageUrl)))
+				Expressions.stringTemplate("GROUP_CONCAT(DISTINCT {0})", accompanyImage.imageUrl),
+				Expressions.stringTemplate(
+					"CONCAT(DATE_FORMAT({0}, '%Y%m%d%H%i%S'), LPAD(CAST({1} AS STRING), 6, '0'))",
+					accompanyBoard.updatedAt,
+					accompanyBoard.id
+				))
+			)
 			.from(accompanyUser)
 			.join(accompanyUser.accompanyBoard, accompanyBoard)
 			.join(accompanyUser.user, user)
 			.leftJoin(accompanyImage).on(accompanyImage.accompanyBoard.id.eq(accompanyBoard.id))
 			.where(isHost())
 			.where(isRegion(region))
+			.where(cursorCondition(cursor, accompanyBoard.updatedAt, accompanyBoard.id))
 			.groupBy(accompanyBoard.id, accompanyBoard.title, accompanyBoard.region,
 				accompanyBoard.startDate, accompanyBoard.endDate, user.nickname)
-			.orderBy(accompanyBoard.updatedAt.desc(), accompanyBoard.createdAt.desc())
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize() + 1)
+			.orderBy(accompanyBoard.updatedAt.desc(), accompanyBoard.id.desc())
+			.limit(size + 1)
 			.fetch();
 
 		boolean hasNext = content.size() > pageable.getPageSize();
@@ -77,7 +88,7 @@ public class AccompanyBoardRepositoryImpl implements AccompanyBoardRepositoryCus
 	}
 
 	@Override
-	public Slice<FindBoardThumbnailsResult> findBoardThumbnailsByUserId(Pageable pageable, Long userId) {
+	public Slice<FindBoardThumbnailsResult> findBoardThumbnailsByUserId(String cursor, int size, Long userId) {
 		List<FindBoardThumbnailsResult> content = queryFactory
 			.select(Projections.constructor(FindBoardThumbnailsResult.class,
 				accompanyBoard.id,
@@ -86,18 +97,24 @@ public class AccompanyBoardRepositoryImpl implements AccompanyBoardRepositoryCus
 				accompanyBoard.startDate,
 				accompanyBoard.endDate,
 				user.nickname,
-				Expressions.stringTemplate("GROUP_CONCAT(DISTINCT {0})", accompanyImage.imageUrl)))
+				Expressions.stringTemplate("GROUP_CONCAT(DISTINCT {0})", accompanyImage.imageUrl),
+				Expressions.stringTemplate(
+					"CONCAT(DATE_FORMAT({0}, '%Y%m%d%H%i%S'), LPAD(CAST({1} AS STRING), 6, '0'))",
+					accompanyUser.updatedAt,
+					accompanyUser.id
+				))
+			)
 			.from(accompanyUser)
 			.join(accompanyUser.accompanyBoard, accompanyBoard)
 			.join(accompanyUser.user, user)
 			.leftJoin(accompanyImage).on(accompanyImage.accompanyBoard.id.eq(accompanyBoard.id))
 			.where(accompanyUser.user.id.eq(userId))
+			.where(cursorCondition(cursor, accompanyUser.updatedAt, accompanyUser.id))
 			.groupBy(accompanyBoard.id, accompanyBoard.title, accompanyBoard.region,
 				accompanyBoard.startDate, accompanyBoard.endDate, user.nickname,
 				accompanyUser.id)
-			.orderBy(accompanyUser.createdAt.desc())
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize() + 1)
+			.orderBy(accompanyUser.updatedAt.desc(), accompanyUser.id.desc())
+			.limit(size + 1)
 			.fetch();
 
 		boolean hasNext = content.size() > pageable.getPageSize();
--- src/main/java/com/dnd/accompany/domain/accompany/infrastructure/querydsl/interfaces/AccompanyBoardRepositoryCustom.java
+++ src/main/java/com/dnd/accompany/domain/accompany/infrastructure/querydsl/interfaces/AccompanyBoardRepositoryCustom.java
@@ -7,9 +7,9 @@ import com.dnd.accompany.domain.accompany.api.dto.FindBoardThumbnailsResult;
 import com.dnd.accompany.domain.accompany.entity.enums.Region;
 
 public interface AccompanyBoardRepositoryCustom {
-	Slice<FindBoardThumbnailsResult> findBoardThumbnails(Pageable pageable, Region region);
+	Slice<FindBoardThumbnailsResult> findBoardThumbnails(String cursor, int size, Region region);
 
-	Slice<FindBoardThumbnailsResult> findBoardThumbnailsByUserId(Pageable pageable, Long userId);
+	Slice<FindBoardThumbnailsResult> findBoardThumbnailsByUserId(String cursor, int size, Long userId);
 
 	boolean isHostOfBoard(Long userId, Long boardId);
 }
