2a886751bcfec634693cec4de02cec533bd062d7
rlarltj <rlarltj1234@gmail.com>
2024-11-09 23:27:08 +0900

    chore: 로그인 시 탈퇴한 유저 데이터는 조회하지 않는다.

--- src/main/java/com/dnd/accompany/domain/user/infrastructure/UserRepository.java
+++ src/main/java/com/dnd/accompany/domain/user/infrastructure/UserRepository.java
@@ -3,9 +3,13 @@ package com.dnd.accompany.domain.user.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dnd.accompany.domain.user.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByProviderAndOauthId(String provider, String oauthId);
+
+    @Query("SELECT u FROM User u WHERE u.provider = :provider AND u.oauthId = :oauthId AND u.deleted = false")
+    Optional<User> findUserByProviderAndOauthId(@Param("provider") String provider, @Param("oauthId") String oauthId);
 }
